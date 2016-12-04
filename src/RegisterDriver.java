981b62a0ecde3b9f6d65b0eb12bdda5162006483
katieg-itt <katie.griffiths@students.ittralee.ie>
2016-12-04 09:27:07 +0000

    Added a RegisterDriver
    Created a Register driver

---
+++ src/RegisterDriver.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Katie on 03/12/2016.
+ */
+
+import out.production.EGame.User;
+
+public class RegisterDriver {
+
+    private static User register() {
+
+        String firstName = "Katie";
+        String lastName = "Griffiths";
+        String username = "SmashingCake";
+        String email = "smashing.cake@gmail.com";
+        String password = "SuperSecretPassword";
+        String phoneNumber = "555 555 555";
+        String address = "Tralee";
+
+        Register newRegister = new Register(firstName, lastName, address, username, password, email, phoneNumber);
+
+        return newRegister.getUser();
+    }
+
+    public static void main(String args[]) {
+        User newUser = register();
+        System.out.println(newUser);
+    }
+}
